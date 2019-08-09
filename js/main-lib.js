65d3bfd0fa90f0123619ee1c9d264330a4564c4c
Linna Liu <cello1412@gmail.com>
2019-08-10 05:48:55 +1000

    edited order

--- js/main-lib.js
+++ js/main-lib.js
@@ -7,40 +7,103 @@ python 3.6 python3 -m http.server
 */
 
 // Helper functions for our 3D sketch
+
 var app = app || {};
 
+app.createSpotlight = () => {
+  const spotlight = new THREE.SpotLight(0xFFFFFF);
+  spotlight.position.set(-10, 100, 50);
 
-app.animate = () => {
-
-  app.animateParticleSystem();
-
-  // Animate the sphere position
-
-  app.sphere.rotation.y += 0.01; //rotationSpeed
-
-  app.cube.rotation.x += 0.01; //rotationSpeed
-  app.cube.rotation.y += 0.01; //rotationSpeed
-  app.cube.rotation.z += 0.01; //rotationSpeed
-
-  app.cubes.forEach(cube => {
-    cube.rotation.x += cube.rotateStep;
-    cube.rotation.y += cube.rotateStep;
-    cube.rotation.z += cube.rotateStep;
-  });
-
-  app.renderer.render(app.scene, app.camera);
-
-  // Get this browser animation API to work out
-  // when to run the animate() method again
-  // (Ideally, 60 times/sec and only when the
-  // window/tab is visible)
-  requestAnimationFrame(app.animate);
-
+  return spotlight;
 };
 
+app.createSphere = () => {
+  const sphereGeometry = new THREE.SphereGeometry(
+    30,  // radius
+    50,  // number of triangle segments on X axis
+    50,  // number of triangle segments on Y axis
+  );
+  
+  const sphereMaterial = new THREE.MeshPhongMaterial({
+    color: 0xffffff,
+    map: new THREE.TextureLoader().load('img/earth.jpg'),
+    // wireframe: true,
+  })
+
+  const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+  sphere.position.set(0, 0, 0);
+  return sphere;
+}
+
+app.createCube = (x, y, z) => {
+  const cubeGeometry = new THREE.BoxGeometry(
+    5, //width
+    5, //height
+    5 //depth
+  );
+
+  const cubeMaterial = new THREE.MeshLambertMaterial({
+    color: 0xffffff,
+  });
+
+  const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+  cube.position.set(x, y, z);
+
+  cube.rotation.x = Math.random();
+  cube.rotation.y = Math.random();
+  cube.rotation.z = Math.random();
+
+  cube.material.color.setRGB(
+    Math.random(),
+    Math.random(),
+    Math.random(),
+  )
+  
+  return cube;
+}
+
+app.createParticleSystem = () => {
+  const particles = new THREE.Geometry();
+  const dist = 200;
+  // Create a particle and give it a random position
+  for (let i = 0; i < 3000; i++) {
+    const particle = new THREE.Vector3(
+      THREE.Math.randInt(-dist, dist), 
+      // 100, //x
+      100, //y
+      // 100, //z
+      THREE.Math.randInt(-dist, dist),
+    );
+
+    // Give each particle a random velocity in x,y,z
+    particle.vx = THREE.Math.randFloat(-1, 1);
+    particle.vy = THREE.Math.randFloat(-1, 1);
+    particle.vz = THREE.Math.randFloat(-1, 1);
+
+    // vertices - The array of vertices holds the position of every vertex in the model.
+    particles.vertices.push(particle);
+  } // for
+
+  const particleMaterial = new THREE.PointsMaterial({
+    color: 0xffffff,
+    size: 10,
+    map: new THREE.TextureLoader().load('img/snowflake.png'),
+    transparent: true,
+    // mixing things together
+    blending: THREE.AdditiveBlending,
+    // Sets the alpha value to be used when running an alpha test. The material will not be renderered if the opacity is lower than this value. Default is 0.
+    alphaTest: 0.5,
+  });
+
+  const particleSystem = new THREE.Points(
+    particles, // the points involved, i.e. the Geometry
+    particleMaterial,
+  )
+
+  return particleSystem;
+}
 
 app.animateParticleSystem = () => {
-
   const particles = app.particleSystem.geometry.vertices;
 
   for (let i = 0; i < particles.length; i++) {
@@ -56,6 +119,8 @@ app.animateParticleSystem = () => {
     // to return)
     if (distSquared > 10.0) {
       // Newton!
+
+      // According to Newton's law of universal gravitation, the attractive force (F) between two point-like bodies is directly proportional to the product of their masses (m1 and m2), and inversely proportional to the square of the distance, r, (inverse-square law) between them:
       const gravityForce = -0.2 * (1.0 / distSquared);
       // Apply the force of gravity to the particle's velocity
       p.vx += gravityForce * p.x;
@@ -70,109 +135,8 @@ app.animateParticleSystem = () => {
     p.x += p.vx * 1.2; //particleVelocityScale
     p.y += p.vy * 1.2;
     p.z += p.vz * 1.2;
-
-  } // for
+  } 
 
   app.particleSystem.geometry.verticesNeedUpdate = true;
 };
 
-
-app.createSpotlight = () => {
-
-  const spotlight = new THREE.SpotLight(0xFFFFFF);
-  spotlight.position.set(-10, 100, 50);
- 
-  return spotlight;
-};
-
-app.createCube = (x, y, z) => {
-
-  const cubeGeometry = new THREE.BoxGeometry(
-    5,
-    5,
-    5
-  );
-  const cubeMaterial = new THREE.MeshLambertMaterial({
-    color: 0xFFFFFF,
-  });
-
-  const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-  cube.position.set(x, y, z);
-
-  cube.rotation.x = Math.random();
-  cube.rotation.y = Math.random();
-  cube.rotation.z = Math.random();
-  cube.rotateStep = Math.random() * 0.05;
-
-  cube.material.color.setRGB(
-    Math.random(),
-    Math.random(),
-    Math.random()
-  );
-
-  return cube;
-};
-
-
-app.createSphere = () => {
-
-  const sphereGeometry = new THREE.SphereGeometry(
-    30,  // radius
-    50,  // number of triangle segments on X axis
-    50,  // number of triangle segments on Y axis
-  );
-  const sphereMaterial = new THREE.MeshPhongMaterial({
-    color: 0xFFFFFF,
-    map: THREE.ImageUtils.loadTexture('img/earth.jpg'),
-    side: THREE.DoubleSide
-  });
-
-  const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
-  sphere.position.set(0, 0, 0);
-
-  return sphere;
-};
-
-
-app.createParticleSystem = () => {
-
-  const particles = new THREE.Geometry();
-  const dist = 200;
-
-  for (let i = 0; i < 3000; i++) {
-    // Create a particle and give it a random position
-    const particle = new THREE.Vector3(
-      THREE.Math.randInt(-dist, dist), // x
-      // 100,
-      // THREE.Math.randInt(-dist, dist), // y
-      100,
-      THREE.Math.randInt(-dist, dist)  // z
-      // 100,
-    );
-
-    // Give each particle a random velocity in x,y,z
-    particle.vx = 0; // THREE.Math.randFloat(-1, 1);
-    particle.vy = 0; // THREE.Math.randFloat(-1, 1);
-    particle.vz = 0; // THREE.Math.randFloat(-1, 1);
-
-    particles.vertices.push(particle);
-  } // for
-
-  const particleMaterial = new THREE.PointsMaterial({
-    color: 0xFFFFFF,
-    size: 10,
-    map: THREE.ImageUtils.loadTexture('img/snowflake.png'),
-    blending: THREE.AdditiveBlending,
-    transparent: true,
-    alphaTest: 0.5
-  });
-
-  console.log({ particles, particleMaterial });
-
-  const particleSystem = new THREE.Points(
-    particles, // the points involved, i.e. the Geometry
-    particleMaterial
-  );
-
-  return particleSystem;
-};
--- js/main.js
+++ js/main.js
@@ -1,74 +1,48 @@
 
 // If the app variable already exists, then reuse it,
 // otherwise initialise it to an empty object
+
 var app = app || {};
 
 // This is the entry point to our application
 app.init = () => {
+  // console.log("hi");
 
-  console.log('Hello!');
-
-  // The scene stores and keeps track of all the objects we are creating, including
-  // the lights and the camera
+  // The scene stores and keeps track of all the objects we are creating, including the lights and the camera
   app.scene = new THREE.Scene();
 
-  // The camera defines our view into the scene, as originating from a
-  // particular perspective
+  // The camera defines our view into the scene, as originating from a particular perspective
   app.camera = new THREE.PerspectiveCamera(
     60, // field of view
-    window.innerWidth / window.innerHeight,  // screen ratio
-    0.1,  // near field (how close to the camera should we still see things)
-    1000  // far field (how far away from the camera should we still see things)
+    window.innerWidth / window.innerHeight, // screen ratio
+    0.1, // near field (how close to the camera should we still see things)
+    1000, // far field (how far away from the camera should we still see things)
   );
-
+  
   // Where exactly is the camera in the scene?
   app.camera.position.x = -100;
-  app.camera.position.y = 200;
-  app.camera.position.z = 50;
-  // app.camera.position.set(-30, 40, 30);
+  app.camera.position.y = 250;
+  app.camera.position.z = 100;
+  // app.camera.position.set(-100, 250, 100);
 
-  app.camera.lookAt(app.scene.position)  // Look at the origin: (x=0, y=0, z=0)
-
-
+  app.camera.lookAt(app.scene.position); // Look at the origin: (x=0, y=0, z=0)
+  
   // The renderer calculates how to draw all the objects in the scene,
   // based on the lighting and the camera perspective, and renders
   // it all down to a 2D image to show in a <canvas> browser element
   app.renderer = new THREE.WebGLRenderer(); // use hardware acceleration!
   app.renderer.setSize(window.innerWidth, window.innerHeight);
-  app.renderer.setClearColor(0x000000);  // background colour
-
-  // shadows are computationally expensive, and thus disabled by default
- 
+  app.renderer.setClearColor(0x000000); // background colour
+  // ----show them when the objects are moving -----
+  app.renderer.setPixelRatio(window.devicePixelRatio || 1);
+  
   // Put the canvas element that the renderer has created into the actual DOM
   document.getElementById('output').appendChild(app.renderer.domElement);
-
+  
   // Add some reference lines for the x,y,z axes (just for debugging)
-  app.axes = new THREE.AxesHelper( 100 );
-  app.scene.add( app.axes );
-
-  app.cube = app.createCube(-50, 100, 80);
-  app.scene.add(app.cube);
-
-  app.cubes = [];
-  for (let i = 0; i < 30; i++) {
-    const range = 60;
-    const cube = app.createCube(
-      THREE.Math.randInt(-range, range), // x
-      // 20,
-      THREE.Math.randInt(-range, range), // y
-      THREE.Math.randInt(-range, range), // z
-    );
-    app.scene.add(cube);
-    app.cubes.push(cube);
-  }
-
-  app.sphere = app.createSphere();
-  app.scene.add(app.sphere);
-
-
-  app.particleSystem = app.createParticleSystem();
-  app.scene.add(app.particleSystem);
-
+  // The X axis is red. The Y axis is green. The Z axis is blue.
+  app.axes = new THREE.AxesHelper(100);
+  app.scene.add(app.axes);
 
   app.spotlight = app.createSpotlight();
   app.scene.add(app.spotlight);
@@ -76,29 +50,93 @@ app.init = () => {
   app.ambient = new THREE.AmbientLight(0x666666);
   app.scene.add(app.ambient);
 
-  // Finally, actually render everything once:
-  // app.renderer.render( app.scene, app.camera );
+  app.sphere = app.createSphere();
+  app.scene.add(app.sphere);
 
+  app.cube = app.createCube(50, 50, 0);
+  app.scene.add(app.cube);
+
+  app.cubes = [];
+  for (let i = 0; i < 30; i++) {
+    const range = 100;
+    const cube = app.createCube(
+      THREE.Math.randInt(-range, range), //x
+      // 20
+      THREE.Math.randInt(-range, range), //y
+      THREE.Math.randInt(-range, range), //z
+    );
+    app.scene.add(cube);
+    app.cubes.push(cube);
+    
+  }
+
+  app.particleSystem = app.createParticleSystem();
+  app.scene.add(app.particleSystem);
+
+  // set initial step to 0
+  app.step = 0;
+  
   // Control camera position with mouse
   app.mouseControls = new THREE.OrbitControls(
     app.camera,
-    app.renderer.domElement
-  );
+    app.renderer.domElement,
+  )
+  
+  // Finally, actually render everything once:
+  // app.renderer.render(app.scene, app.camera);
 
-  app.animate();  // start the animation loop
+  // ----- replace renderer by animate() to keep updating the camera perspective ----
+  app.animate(); // start the animation loop
 
-}; // init()
+} // init()
 
 // Like jQuery $(document).ready()
 window.onload = app.init;
 
 
+app.animate = () => {
+  app.animateParticleSystem();
+
+  // Animate the sphere position
+
+  app.step += 0.05;
+
+  // --------- Maths! ----------
+  // app.sphere.position.x = Math.cos(app.step) * 20;
+  // app.sphere.position.y = 30 + Math.abs(Math.sin(app.step) * 20);
+  // app.sphere.position.y += 0.1;
+
+  app.sphere.rotation.y += 0.01; //rotationSpeed
+
+  app.cube.rotation.x += 0.01;
+  app.cube.rotation.y += 0.01;
+  app.cube.rotation.z += 0.01;
+
+  app.cubes.forEach(cube => {
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    cube.rotation.z += 0.01;
+  })
+
+  app.renderer.render(app.scene, app.camera);
+
+  // Get this browser animation API to work out
+  // when to run the animate() method again
+  // (Ideally, 60 times/sec and only when the
+  // window/tab is visible)
+
+  requestAnimationFrame(app.animate);
+}
+
 app.resize = () => {
   app.camera.aspect = window.innerWidth / window.innerHeight;
-  app.camera.updateProjectionMatrix();
-
   app.renderer.setSize(window.innerWidth, window.innerHeight);
-};
+
+  // after making changes to most of the camera's properties you will have to call .updateProjectionMatrix for the changes to take effect.
+  app.camera.updateProjectionMatrix();
+  // console.log("the window size has changed");
+}
 
 // window.onresize = app.init;  // overwrites any previous onresize handler
 window.addEventListener('resize', app.resize);
+
